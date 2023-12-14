9610cdb8b0d7f10eae18d889390cb88d15c378cd
huangz826 <huang.zix@northeastern.edu>
2023-12-14 13:32:12 -0800

    completed fetching favorite movies from home page

--- films-list-react-web-app/src/components/home/index.js
+++ films-list-react-web-app/src/components/home/index.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from "react";
 import { fetchTrendingMovies } from "../../services/movieService";
 import MovieGrid from "../movies/MovieGrid";
 import { useSelector } from "react-redux";
+import { getMovieDetailsById } from "../../services/movieService";
 
 const Home = () => {
   // TODO: FETCH CURRENT USER
@@ -17,7 +18,16 @@ const Home = () => {
   };
 
   // TODO: FETCH FAVORITE MOVIES
-  const getFavoriteMovies = async (user) => {};
+  const getFavoriteMovies = async (user) => {
+    try {
+      const favoriteMovieDetails = await Promise.all(
+        user.favoriteMovies.map((movieId) => getMovieDetailsById(movieId))
+      );
+      setFavoriteMovies(favoriteMovieDetails);
+    } catch (error) {
+      // TODO: ERROR HANDLING
+    }
+  };
 
   const [trendingMovies, setTrendingMovies] = useState([]);
   const [favoriteMovies, setFavoriteMovies] = useState([]);
